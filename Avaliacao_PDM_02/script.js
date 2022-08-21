787c4792ca6b030a4547e9ee1ede6623b68d6710
Willian <92097335+Will448@users.noreply.github.com>
2022-08-21 18:23:34 -0300

    Add files via upload

---
+++ Avaliacao_PDM_02/script.js
@@ -0,0 +1,31 @@
+/*console.log("Hello world");
+
+var dados = {
+/* Uma maneira de ser realizada:
+nome: document.getElementById("nome1").value,
+telefone: parseInt(document.getElementById("telefone1").value),
+salario: parseFloat(document.getElementById("salario1").value)
+nome: prompt("Coloque o seu nome"),
+idade: parseInt(prompt("Coloque a sua idade")),
+cidade: prompt("Coloque a sua cidade")
+}
+ 
+  //parseInt(document.getElementById("reajuste1").value);
+ if(dados.idade <= 18) {
+    alert("Você ainda é de menor e jovem fdp");
+    document.getElementById("nome").innerHTML = dados.nome;
+    document.getElementById("idade").innerHTML = dados.idade;
+    document.getElementById("cidade").innerHTML = dados.cidade;  
+  console.log(dados.nome);
+  console.log(dados.idade);
+  console.log(dados.cidade);
+ } else {
+    alert("Cuidado!🔴🚨 com o lugar que você coloca suas informações");
+    document.getElementById("nome").innerHTML = dados.nome;
+    document.getElementById("idade").innerHTML = dados.idade;
+    document.getElementById("cidade").innerHTML = dados.cidade;  
+console.log("Seu nome é: " + dados.nome);
+console.log("Sua idade é: " + dados.idade);
+console.log("Sua cidade é: " + dados.cidade);
+ }
+*/
